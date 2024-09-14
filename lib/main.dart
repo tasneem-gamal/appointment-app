79f8023ccae200481504023ebaa080f0359221d0
tasneem-gamal <tasneemgamal044@gmail.com>
2024-09-14 15:43:00 -0700

    tst

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AppointApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppointApp extends StatelessWidget {
+  const AppointApp({super.key});
 
   @override
   Widget build(BuildContext context) {

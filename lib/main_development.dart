4e8ccaaf414c2ee86310ca1f72e87c4a08466461
tasneem-gamal <tasneemgamal044@gmail.com>
2024-09-23 12:21:07 -0700

    create dev and production flavors for android

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,9 +2,12 @@ import 'package:appointment_app/core/di/dependency_injection.dart';
 import 'package:appointment_app/core/routing/app_router.dart';
 import 'package:appointment_app/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   setupGetIt();
+  // to fix .sp font size in flutter_screen util in release mode
+  ScreenUtil.ensureScreenSize;
   runApp(DocApp(
     appRouter: AppRouter(),
   ));

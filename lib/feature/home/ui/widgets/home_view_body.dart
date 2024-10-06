bd7ec993c0d94e44ab6f97c9e1ba6baa96ed1437
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-06 11:46:12 -0700

    delete doctor speciality see all

--- lib/feature/home/ui/widgets/home_view_body.dart
+++ lib/feature/home/ui/widgets/home_view_body.dart
@@ -3,9 +3,9 @@ import 'package:appointment_app/feature/home/ui/widgets/find_nearby_doc_containe
 import 'package:appointment_app/feature/home/ui/widgets/speciality_section/doctor_specility_bloc_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../core/theming/styles.dart';
 import 'recommendation_doctors_section/recommendation_doctors_bloc_builder.dart';
 import 'recommendation_doctors_section/recommendation_see_all.dart';
-import 'speciality_section/doctor_specility_see_all.dart';
 import 'home_view_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -17,21 +17,25 @@ class HomeViewBody extends StatelessWidget {
       padding: Constants.homepadding,
       child: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const HomeViewAppBar(),
             SizedBox(height: 30.h),
             const FindNearbyDocContainer(),
             SizedBox(height: 24.h),
-            const DoctorSpecilitySeeAll(),
+            Text(
+              'Doctor Speciality',
+              style: Styles.textStyle18.copyWith(fontWeight: FontWeight.w600),
+            ),
             SizedBox(height: 16.h),
             const DoctorSpecilityBlocBuilder(),
             SizedBox(
               height: 16.h,
             ),
             const RecommendationSeeAll(),
-                SizedBox(
-                  height: 12.h,
-                ),
+            SizedBox(
+              height: 12.h,
+            ),
             const RecommendationDoctorsBlocBuilder()
           ],
         ),

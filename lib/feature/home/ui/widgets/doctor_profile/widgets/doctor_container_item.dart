2e03a125250b98345d6e981fee54dfeb153cf8ed
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-09 11:14:45 -0700

    create doctor profile item

---
+++ lib/feature/home/ui/widgets/doctor_profile/widgets/doctor_container_item.dart
@@ -0,0 +1,74 @@
+import 'package:appointment_app/core/theming/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../../core/theming/styles.dart';
+
+class DoctorContainerItem extends StatelessWidget {
+  const DoctorContainerItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 79.h,
+      decoration: BoxDecoration(
+          color: ColorsManager.kNotifiBackgroundGrey,
+          borderRadius: BorderRadius.circular(16)),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              'assets/images/recom_doc.png', // there is no image for doc
+              width: 74.w,
+              height: 74.h,
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(
+            width: 16.h,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Dr. Randy Wigham',
+                  style:
+                      Styles.textStyle16.copyWith(fontWeight: FontWeight.w700),
+                ),
+                SizedBox(
+                  height: 8.h,
+                ),
+                Text('General | RSUD Gatot Subroto',
+                    style: Styles.textStyle12.copyWith(
+                        fontWeight: FontWeight.w500,
+                        color: ColorsManager.kGreytextColor)),
+                SizedBox(
+                  height: 12.h,
+                ),
+                Text(
+                  "hello@gmail.com",
+                  style: Styles.textStyle12.copyWith(
+                      fontWeight: FontWeight.w500,
+                      color: ColorsManager.kGreytextColor),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 50),
+            child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.message,
+                  color: ColorsManager.kPrimaryColor,
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/feature/home/ui/widgets/doctor_profile/widgets/doctor_profile_body.dart
+++ lib/feature/home/ui/widgets/doctor_profile/widgets/doctor_profile_body.dart
@@ -1,6 +1,7 @@
 import 'package:appointment_app/core/helpers/constants.dart';
+import 'package:appointment_app/feature/home/ui/widgets/doctor_profile/widgets/doctor_container_item.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../see_all_recommendation_doctors/widgets/recommendation_doctor_and_profile_top_bar.dart';
 
 class DoctorProfileBody extends StatelessWidget {
@@ -10,9 +11,11 @@ class DoctorProfileBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: Constants.homepadding,
-      child: const Column(
+      child: Column(
         children: [
-          RecommendationDoctorAndProfileTopBar(topBarText: 'Dr Randy Wigham')
+          const RecommendationDoctorAndProfileTopBar(topBarText: 'Dr Randy Wigham'),
+          SizedBox(height: 32.h,),
+          const DoctorContainerItem()
         ],
       ),
     );

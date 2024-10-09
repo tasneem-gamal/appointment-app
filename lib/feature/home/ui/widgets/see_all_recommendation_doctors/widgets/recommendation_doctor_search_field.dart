f25e06c1b67cb7f8424ad469d2686b00dacdb66a
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-09 10:39:39 -0700

    remove sort icon

--- lib/feature/home/ui/widgets/see_all_recommendation_doctors/widgets/recommendation_doctor_search_field.dart
+++ lib/feature/home/ui/widgets/see_all_recommendation_doctors/widgets/recommendation_doctor_search_field.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../../../core/theming/colors.dart';
@@ -17,48 +16,31 @@ class RecommendationDoctorSearchField extends StatelessWidget {
   Widget build(BuildContext context) {
   final TextEditingController textEditingController = TextEditingController();
 
-    return Row(
-      children: [
-        SizedBox(
-          width: 290.w,
-          height: 42.h,
-          child: TextFormField(
-            onChanged: (value){
-              BlocProvider.of<SearchCubit>(context).searchForDoctors(value);
-            },
-            controller: textEditingController,
-            decoration: InputDecoration(
-              hintText: 'Search',
-              hintStyle: const TextStyle(
-                color: ColorsManager.kGreySecondaryTextColor,
-              ),
-              prefixIcon: const Icon(
-                FontAwesomeIcons.magnifyingGlass,
-                color: ColorsManager.kGreySecondaryTextColor,
-              ),
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: const BorderSide(color: Color(0xffEDEDED))),
-              enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: const BorderSide(color: Color(0xffEDEDED))),
-              focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: const BorderSide(
-                      color: ColorsManager.k12SizeColorGreyShade)),
-            ),
-          ),
+    return TextFormField(
+      onChanged: (value){
+        BlocProvider.of<SearchCubit>(context).searchForDoctors(value);
+      },
+      controller: textEditingController,
+      decoration: InputDecoration(
+        hintText: 'Search',
+        hintStyle: const TextStyle(
+          color: ColorsManager.kGreySecondaryTextColor,
         ),
-        Expanded(
-          child: IconButton(
-            onPressed: (){}, 
-            icon: const Icon(
-              Icons.filter_list,
-              size: 30,
-            )
-          ),
-        )
-      ],
+        prefixIcon: const Icon(
+          FontAwesomeIcons.magnifyingGlass,
+          color: ColorsManager.kGreySecondaryTextColor,
+        ),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(color: Color(0xffEDEDED))),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(color: Color(0xffEDEDED))),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(
+                color: ColorsManager.k12SizeColorGreyShade)),
+      ),
     );
   }
 }

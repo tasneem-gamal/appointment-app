e10935ba67aa31f7a909174834414733075a57c5
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-09 11:01:27 -0700

    refactor

--- lib/feature/home/ui/widgets/doctor_profile/widgets/doctor_profile_body.dart
+++ lib/feature/home/ui/widgets/doctor_profile/widgets/doctor_profile_body.dart
@@ -1,6 +1,8 @@
 import 'package:appointment_app/core/helpers/constants.dart';
 import 'package:flutter/material.dart';
 
+import '../../see_all_recommendation_doctors/widgets/recommendation_doctor_and_profile_top_bar.dart';
+
 class DoctorProfileBody extends StatelessWidget {
   const DoctorProfileBody({super.key});
 
@@ -8,9 +10,9 @@ class DoctorProfileBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: Constants.homepadding,
-      child: Column(
+      child: const Column(
         children: [
-          
+          RecommendationDoctorAndProfileTopBar(topBarText: 'Dr Randy Wigham')
         ],
       ),
     );

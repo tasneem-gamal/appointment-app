962500ba8a7e36ac34b4ca45e77e0091e3f40a3c
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-16 13:29:53 -0700

    refactor

--- lib/feature/search/widgets/search_view_body.dart
+++ lib/feature/search/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:appointment_app/core/helpers/constants.dart';
+import 'package:appointment_app/core/widgets/custom_form_field_for_search.dart';
 import 'package:appointment_app/core/widgets/custom_top_bar.dart';
+import 'package:appointment_app/feature/search/widgets/category_chip_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -15,7 +17,9 @@ class SearchViewBody extends StatelessWidget {
         children: [
           const CustomTopBar(topBarText: 'Search'),
           SizedBox(height: 32.h,),
-          
+          const CustomTextFormFieldForsearch(),
+          SizedBox(height: 24.h,),
+          const CategoryChipListView()
         ],
       ),
     );

596056ccb83bd06b5d88b89831c48115c6b87946
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-18 15:12:33 -0700

    refactor

--- lib/feature/search/widgets/category_chip_list_view.dart
+++ lib/feature/search/widgets/category_chip_list_view.dart
@@ -2,17 +2,45 @@ import 'package:appointment_app/feature/search/widgets/chip_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
-class CategoryChipListView extends StatelessWidget {
+class CategoryChipListView extends StatefulWidget {
   const CategoryChipListView({super.key});
-  
+
+  @override
+  State<CategoryChipListView> createState() => _CategoryChipListViewState();
+}
+
+class _CategoryChipListViewState extends State<CategoryChipListView> {
+  int selectedIndex = 0;
+
+  final List<String> chipLabels = [
+    'All',
+    'General',
+    'Cardiology',
+    'Neurology',
+    'Orthopedics',
+    'Pediatrics',
+    'Gynecology',
+    'Ophthalmology',
+    'Urology',
+    'Gastroenterology',
+    'Psychiatry'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 41.h,
       child: ListView.builder(
-        itemBuilder: (context, index) => const ChipItem(),
-        itemCount: 10,
+        itemBuilder: (context, index) => ChipItem(
+          label: chipLabels[index],
+          isSelected: selectedIndex == index,
+          onSelected: (){
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+        ),
+        itemCount: chipLabels.length, 
         scrollDirection: Axis.horizontal,
       ),
     );
--- lib/feature/search/widgets/chip_item.dart
+++ lib/feature/search/widgets/chip_item.dart
@@ -1,32 +1,43 @@
+import 'package:appointment_app/core/theming/colors.dart';
 import 'package:appointment_app/core/theming/styles.dart';
 import 'package:flutter/material.dart';
 
 class ChipItem extends StatefulWidget {
   const ChipItem({
-    super.key
+    super.key, required this.label, required this.isSelected, required this.onSelected
   });
-
-
+  final String label;
+  final bool isSelected;
+  final VoidCallback onSelected;
   @override
   State<ChipItem> createState() => _ChipItemState();
 }
 
 class _ChipItemState extends State<ChipItem> {
-  bool isSelected = false;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 8),
-      child: Chip(
-        label: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-          child: Text(
-            'General',
-            style: Styles.textStyle14,
+      child: GestureDetector(
+        onTap: widget.onSelected,
+        child: Chip(
+          backgroundColor: widget.isSelected ? ColorsManager.kPrimaryColor : ColorsManager.kLighterGrey,
+          label: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            child: Text(
+              widget.label,
+              style: Styles.textStyle14.copyWith(
+                color: widget.isSelected ? Colors.white : Colors.black,
+              ),
+            ),
+          ),
+          shape: StadiumBorder(
+            side: BorderSide(
+              color: widget.isSelected ? ColorsManager.kPrimaryColor : ColorsManager.kGreySecondaryTextColor,
+            )
           ),
         ),
-        shape: StadiumBorder(),
       ),
     );
   }
